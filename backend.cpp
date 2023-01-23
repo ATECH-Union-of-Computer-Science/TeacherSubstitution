3fb04037363c1a51ef9065284e09e292660942f7
Anthony Saldana Valle <55422282+ansavanix@users.noreply.github.com>
2023-01-23 10:12:19 -0800

    Create teacher struct

--- backend.cpp
+++ backend.cpp
@@ -1,1 +1,18 @@
 #include <fstream>
+
+using namespace std;
+
+struct Teacher {
+	string name;
+	string type;
+	string room;
+	vector<string> periods;
+}
+
+int main() {
+	
+	//Parse spreadsheet
+	
+	
+	return 0;
+}
