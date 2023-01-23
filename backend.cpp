3dcfe6b9cbc9f6b44ba2565bc8a4d5e44f744326
Anthony Saldana Valle <55422282+ansavanix@users.noreply.github.com>
2023-01-23 10:29:23 -0800

    Findsubs shell

--- backend.cpp
+++ backend.cpp
@@ -10,15 +10,31 @@ struct Teacher {
 	vector<string> periods;
 }
 
+int findTeacher(const vector<Teacher>& teachers, const string& name) {
+	//Returns the index of the teacher with the given name. (Junjie)
+	//If not found return -1
+	
+}
+
+vector<int> findSubs(const Teacher& teacher, vector<Teacher>& teachers) {
+	//Returns a vector of ints containing the index of the teacher that will fill in for an absent teacher.
+	//If no teacher is available to sub that period, place a -1 in the index.
+	//If a period is a prep for an absent teacher, place a -2 in the index.
+	//Replace teacher's preps with the phrase "SUB" to mark preps that have already been used.
+	
+}
+
 int main() {
 	vector<Teacher> teachers;
-	//Parse spreadsheet
+	//Parse spreadsheet (Henry)
 	
 	
-	//User interface
+	//User interface (Charles)
 	while (true) {
 		
 	}
+	
+	
 	
 	return 0;
 }
